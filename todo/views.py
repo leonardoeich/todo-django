c06cb880e7e98e792ccd1664ce53ccae0cde3daa
Leonardo Eich <leo.eich@hotmail.com>
2023-11-21 20:55:00 -0300

    finish crud operations

--- todo/views.py
+++ todo/views.py
@@ -19,3 +19,21 @@ def mark_as_undone(request, pk):
   task.is_completed = False
   task.save()
   return redirect('home')
+
+def edit_task(request, pk):
+  task = get_object_or_404(Task, pk=pk)
+  if request.method == 'POST':
+    new_task = request.POST['task']
+    task.task = new_task
+    task.save()
+    return redirect('home')
+  else:
+    context = {
+      "task": task,
+    }
+    return render(request, 'edit_task.html', context )
+
+def delete_task(request, pk):
+  task = get_object_or_404(Task, pk=pk)
+  task.delete()
+  return redirect('home')
